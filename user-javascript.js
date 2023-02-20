dd5d03edc5d714c6780ebc71454734aad43a9934
Anxus <89088411+0xarch@users.noreply.github.com>
2023-02-20 10:42:22 +0800

    Add files via upload

--- user-javascript.js
+++ user-javascript.js
@@ -1,16 +1,25 @@
-    if(window.location.host=="cn.bing.com"){
-        var link=document.createElement('link');
-        link.rel="stylesheet";
-        link.type="text/css";
+var link=document.createElement('link');
+link.rel="stylesheet";
+link.type="text/css";
+if(window.location.host=="cn.bing.com"){
+        //var link=document.createElement('link');
+        //link.rel="stylesheet";
+        //link.type="text/css";
         link.href='https://cdn.jsdelivr.net/china/gh/0xarch/epiphany_css/css_bing.css';
         document.querySelector("head").appendChild(link);
         console.log("UPLOAD BING CSS");
     }
-    if(window.location.host.includes(".bilibili.com")){
-        var link=document.createElement('link');
-        link.rel="stylesheet";
-        link.type="text/css";
+else if(window.location.host.includes(".bilibili.com")){
+        //var link=document.createElement('link');
+        //link.rel="stylesheet";
+        //link.type="text/css";
         link.href='https://cdn.jsdelivr.net/china/gh/0xarch/epiphany_css/css_bili.css';
         document.querySelector("head").appendChild(link);
         console.log("UPLOAD BILI CSS");
     }
+else if(window.location.host.inclues(".zhihu.com")){
+        link.href='https://cdn.jsdelivr.net/china/gh/0xarch/epiphany_css/css_zhihu.css';
+        document.querySelector("head").appendChild(link);
+        console.log("UPLOAD ZHIHU CSS");
+}
+	
